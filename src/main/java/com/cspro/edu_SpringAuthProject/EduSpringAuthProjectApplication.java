ded27abb4f50767836340c1c6cccc7fc3af6d085
NICKmop <dlckdtjq153@gmail.com>
2024-10-10 10:52:10 +0900

    init project

---
+++ src/main/java/com/cspro/edu_SpringAuthProject/EduSpringAuthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cspro.edu_SpringAuthProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSpringAuthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSpringAuthProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cspro/edu_SpringAuthProject/EduSpringAuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cspro.edu_SpringAuthProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSpringAuthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
